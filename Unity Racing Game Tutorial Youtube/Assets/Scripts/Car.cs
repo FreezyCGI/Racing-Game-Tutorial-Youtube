dee6e7a4378f94b31f46c31a8193ad54b7b13beb
FreezyCGI <patrick.fritzl@gmx.at>
2022-10-12 01:18:09 +0200

    4_129_rigid.addforce

--- Unity Racing Game Tutorial Youtube/Assets/Scripts/Car.cs
+++ Unity Racing Game Tutorial Youtube/Assets/Scripts/Car.cs
@@ -4,18 +4,25 @@ using UnityEngine;
 
 public class Car : MonoBehaviour
 {
+    Rigidbody rigid;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigid = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.W))
-        {
-            transform.position += new Vector3(0.01f, 0, 0);
-        }
+        //if(Input.GetKey(KeyCode.W))
+        //{
+        //    transform.position += transform.forward * 0.01f;
+        //}
+        //if (Input.GetKey(KeyCode.S))
+        //{
+        //    transform.position += transform.forward * -0.01f;
+        //}
+        float inputVertical = Input.GetAxis("Vertical");
+        rigid.AddForce(transform.forward * inputVertical);
     }
 }
