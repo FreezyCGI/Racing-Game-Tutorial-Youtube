52d48527ea0069f8b8db9267e94a0fefdaa39245
FreezyCGI <patrick.fritzl@gmx.at>
2022-11-06 18:27:26 +0100

    6_121_ending

--- Unity Racing Game Tutorial Youtube/Assets/Scripts/Car.cs
+++ Unity Racing Game Tutorial Youtube/Assets/Scripts/Car.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Car : MonoBehaviour
 {
+    public Transform centerOfMass;
+    public List<Axis> Axles;
     public float acceleration = 1;
     public float rotationSpeed = 1;
     Rigidbody rigid;
@@ -11,6 +13,7 @@ public class Car : MonoBehaviour
     void Start()
     {
         rigid = GetComponent<Rigidbody>();
+        rigid.centerOfMass = centerOfMass.localPosition;
     }
 
     // Update is called once per frame
@@ -24,10 +27,35 @@ public class Car : MonoBehaviour
         //{
         //    transform.position += transform.forward * -0.01f;
         //}
+
+        //rigid.AddForce(transform.forward * inputVertical * Time.deltaTime * acceleration);
+        //transform.Rotate(new Vector3(0, inputHorizontal * Time.deltaTime * rotationSpeed, 0));
+
         float inputVertical = Input.GetAxis("Vertical");
         float inputHorizontal = Input.GetAxis("Horizontal");
 
-        rigid.AddForce(transform.forward * inputVertical * Time.deltaTime * acceleration);
-        transform.Rotate(new Vector3(0, inputHorizontal * Time.deltaTime * rotationSpeed, 0));
+        foreach(Axis axis in Axles)
+        {
+            if(axis.canAccelerate)
+            {
+                axis.leftWheel.motorTorque = inputVertical * acceleration;
+                axis.rightWheel.motorTorque = inputVertical * acceleration;
+            }
+            if (axis.canSteer)
+            {
+                axis.leftWheel.steerAngle = inputHorizontal * rotationSpeed;
+                axis.rightWheel.steerAngle = inputHorizontal * rotationSpeed;
+            }
+        }
+
     }
+}
+
+[System.Serializable]
+public class Axis
+{
+    public WheelCollider leftWheel;
+    public WheelCollider rightWheel;
+    public bool canAccelerate;
+    public bool canSteer;
 }
